edbc6c0399c4480d296a6195d0163e7634776075
edwardzhong <edwardloveyou@gmail.com>
2020-01-06 12:50:00 +0800

    focus

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import '../public/index.css'
 import '../public/iconfont.js'
 
 Vue.config.productionTip = false
-
+Vue.directive('focus', {
+  inserted: function (el) {
+    el.focus()
+  }
+})
 new Vue({
   router,
   store,
